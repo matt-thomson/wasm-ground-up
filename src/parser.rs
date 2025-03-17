e7ce52320eea460ffc794a8725cbcae51a2664a3
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2025-03-17 15:35:34 +0000

    Parse numbers

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,9 @@ mod tests {
     use pest::Parser as PestParser;
 
     #[test]
-    fn should_parse_nop() {
-        Parser::parse(Rule::main, "").expect("failed to parse");
+    fn should_parse_numbers() {
+        Parser::parse(Rule::main, "123").expect("failed to parse 123");
+        Parser::parse(Rule::main, "42").expect("failed to parse 42");
+        Parser::parse(Rule::main, "abc").expect_err("failed to error when parsing abc");
     }
 }
