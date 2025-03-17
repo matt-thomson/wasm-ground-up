3195908cfa1cc4fccca4449535e9a659a5f53c84
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2025-03-17 15:51:08 +0000

    Encapsulate parsing

--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,32 @@
+use pest::Parser as PestParser;
+use pest::iterators::Pairs;
+
 #[derive(pest_derive::Parser)]
 #[grammar = "wafer.pest"]
-pub struct Parser;
+struct Parser;
+
+pub struct Wafer<'a>(Pairs<'a, Rule>);
+
+impl<'a> Wafer<'a> {
+    pub fn parse(input: &'a str) -> Self {
+        let parsed = Parser::parse(Rule::main, input).expect("failed to parse");
+
+        Self(parsed)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::{Parser, Rule};
-    use pest::Parser as PestParser;
+    use super::Wafer;
 
     #[test]
     fn should_parse_numbers() {
-        Parser::parse(Rule::main, "123").expect("failed to parse 123");
-        Parser::parse(Rule::main, "42").expect("failed to parse 42");
-        Parser::parse(Rule::main, "abc").expect_err("failed to error when parsing abc");
+        Wafer::parse("123");
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_fail_to_parse_non_numeric() {
+        Wafer::parse("abc");
     }
 }
