48b5211f62dfeb5c612490e135375e47fca91fd7
Matt Thomson <771863+matt-thomson@users.noreply.github.com>
2025-03-17 09:43:58 +0000

    Generate void WASM

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+fn compile_void_lang(input: &str) -> Vec<u8> {
+    if !input.is_empty() {
+        panic!("Expected empty code, got {}", input);
+    }
+
+    vec![0, 97, 115, 109, 1, 0, 0, 0]
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use wasmi::{Engine, Instance, Module, Store};
+
+    use super::compile_void_lang;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn should_compile_void_lang() {
+        let wasm = compile_void_lang("");
+
+        let engine = Engine::default();
+        let module = Module::new(&engine, wasm).expect("couldn't parse module");
+        let store = Store::new(&engine, 0);
+
+        Instance::new(store, &module, &[]).expect("couldn't build instance");
     }
 }
